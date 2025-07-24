1ee3f5601f092f7d06f82c2664ce07df04eb9ce3
jzhng105 <jzhng105@gmai.com>
2025-07-23 23:28:31 -0400

    Changes to be committed: 	modified:   actstats/__init__.py 	modified:   actstats/core.py 	new file:   actstats/utils/__init__.py 	new file:   actstats/utils/utils.py 	modified:   pyproject.toml

--- actstats/core.py
+++ actstats/core.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.stats as stats
-import datetime
 from actstats.scipy_decorators import poisson, nbinom
+from actstats.utils.utils import fraction_to_date_full
 
 class NHPPDistribution:
     """
@@ -240,23 +240,6 @@ class ActuarialClass:
 
 # Create an instance of the ActuarialClass
 actuarial = ActuarialClass()
-
-# --- Helper Function to Convert Fraction to Date ---
-def fraction_to_date_full(t, year=2024):
-    """
-    Converts a fraction of a year (0 <= t < 1) into a date (month and day)
-    for a given year. Accounts for leap years.
-    """
-    start_date = datetime.date(year, 1, 1)
-    # Determine number of days in the year
-    if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
-        days_in_year = 366
-    else:
-        days_in_year = 365
-    # Convert fraction to day offset (0-indexed)
-    day_offset = int(t * days_in_year)
-    event_date = start_date + datetime.timedelta(days=day_offset)
-    return event_date
 
 if __name__ == "__main__":
     np.random.seed(42)  # Set seed for NumPy
